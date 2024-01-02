bed64a7acf1b78e9ea4bf660e53e786c3afa2db9
vorapat0881 <vorapat_g@cmu.ac.th>
2024-01-02 17:18:27 +0700

    lab13_2

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,28 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void showImage(const bool SI[][M]){
+    for(int i=0; i < M+2; i++) cout << "-";
+    cout << "\n";
+
+    for(int j = 0; j < N; j++ ){
+        cout << "|";
+        for(int k=0; k < M; k++){
+            if(SI[j][k] == 1) cout << "*";
+            else cout << " ";
+        }
+        cout << "|\n";
+    }
+    for(int i=0; i < M+2; i++) cout << "-";
+    cout << "\n";
+
+  
+  
+}
+
+void updateImage( bool image[N][M],int s,int x,int y){
+   for(int i=0; i < N; i++ )
+       for(int j=0; j < M; j++)
+           if(sqrt(pow((i-x),2)+pow((j-y),2)) <= s-1)
+               image[i][j] = 1; 
+}
